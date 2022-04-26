da774feb67a610525db6704885e9ab2fab5737d6
deelnase <Makimova2003@gmail.com>
2022-04-26 20:41:18 +0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.demo6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    opens com.example.demo6 to javafx.fxml;
+    exports com.example.demo6;
+}
